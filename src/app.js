9c5c0961399b3d56960768041331fbd3854f300a
rasi <rasi@rasis-mbp.home>
2024-01-04 22:07:00 -0500

    added routes and error page

--- src/app.js
+++ src/app.js
@@ -2,15 +2,36 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+// createBrowserRouter is a function in React Router used to define a set of routes for a web application, mapping URL paths to corresponding React components. RouterProvider is a component that takes the router instance created by createBrowserRouter and enables routing functionality in the application by wrapping the app or a part of it in the routing context.
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
 
 const AppLayout = () => {
   return (
     <div className="app">
       <Header />
-      <Body />
+      <Outlet />
     </div>
   );
 };
 
+
+// client side v/s server side routing
+// Client-side routing in React involves handling the navigation between different views within a single-page application directly in the browser, without making new requests to the server for each page change. Server-side routing, on the other hand, refers to the traditional method where navigation requests are processed by the server, which sends back a new page each time.
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      { path: "/", element: <Body />},
+      { path: "/about", element: <About /> },
+      { path: "/contact", element: <Contact /> },
+    ],
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
